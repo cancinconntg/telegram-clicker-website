5dcedebb7768379d1d9f4ded36ae84faca0ee01f
bohdan-zborovskyi-repo <bohdan.zborovskyi@gmail.com>
2024-06-10 21:28:42 +0200

    minor fix

--- routes/userinformation/addUserInformation.js
+++ routes/userinformation/addUserInformation.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { addUserIfNotExists } = require('../../database/dbOperations');
 
-router.post('/', async (req, res) => {d
+router.post('/', async (req, res) => {
     const data = req.body;
     if (!data.telegramId || !data.telegramUsername) {
         return res.status(400).json({ error: 'Missing required fields' });
